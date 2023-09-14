2f3d51b4efe64fbb2abd0a96217a9153f4aafd8c
Rahul Dev <rdev.dev01@gmail.com>
2023-09-14 17:15:07 +0530

    Bottom Navigation and Products data from api

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,39 +1,59 @@
-import { View, Text , StyleSheet, Dimensions} from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const {height, width} = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 const Header = ({
-    title,
-    leftIcon,
-    rightIcon,
-    onClickLeftIcon,
-    onClickRightIcon,
+  title,
+  leftIcon,
+  rightIcon,
+  onClickLeftIcon,
+  onClickRightIcon,
 }) => {
   return (
     <View style={styles.header}>
+      <TouchableOpacity style={styles.btn} onPress={()=>{
+        onClickLeftIcon();
+      }}>
+        <Image source={leftIcon} style={styles.icon} />
+      </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
       <TouchableOpacity style={styles.btn}>
-
+        <Image source={rightIcon} style={[styles.icon,{width:40, height:40}]} />
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-    header:{
-        width: width,
-        height:60,
-        backgroundColor: '#0786DAFD',
-        flexDirection:'row',
-        justifyContent:'space-between',
-    },
-    btn: {
-        width:40,
-        height:40,
-        justifyContent:"center",
-        alignContent:'center',
-    },
-})
+  header: {
+    width: width,
+    height: 65,
+    backgroundColor: "#0786DAFD",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems:'center',
+    // paddingLeft:15,
+    // paddingRight:15,
+    // paddingTop:15,
+    padding:15,
+  },
+  btn: {
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    tintColor: '#fff',
+  },
+  title:{
+    color:'#fff',
+    fontSize: 20,
+  }
+});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,97 @@
-import { View, Text,StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { React, useState } from "react";
 import Header from "../common/Header";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+import Home from "./tabs/Home";
+import Search from "./tabs/Search";
+import Notification from "./tabs/Notification";
+import User from "./tabs/User";
+import Wishlist from "./tabs/Wishlist";
 
 const HomeScreen = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <View style={styles.container}>
-      <Header/>
+      {/* <Header 
+      leftIcon={require('../images/menu.png')}
+      rightIcon={require('../images/cart.png')}
+      title={'Grocery App'}
+      /> */}
+
+      {selectedTab == 0 ? (
+        <Home />
+      ) : selectedTab == 1 ? (
+        <Search />
+      ) : selectedTab == 2 ? (
+        <Wishlist />
+      ) : selectedTab == 3 ? (
+        <Notification />
+      ) : (
+        <User />
+      )}
+
+      <View style={styles.bottomView}>
+        {/* Here we are making the bottom navigation */}
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(0);
+          }}
+        >
+          <Image
+            source={selectedTab == 0 ? require("../images/home_fill.png") : require("../images/home.png") }
+            style={styles.bottomTabIcon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(1);
+          }}
+        >
+          <Image
+            source={require("../images/search.png")}
+            style={styles.bottomTabIcon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(2);
+          }}
+        >
+          <Image
+            source={selectedTab == 2 ? require("../images/wishlist_fill.png") : require("../images/wishlist.png")}
+            style={styles.bottomTabIcon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(3);
+          }}
+        >
+          <Image
+            source={selectedTab == 3 ? require("../images/noti_fill.png"): require("../images/noti.png")}
+            style={styles.bottomTabIcon}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(4);
+          }}
+        >
+          <Image
+            source={selectedTab == 4 ? require("../images/user_fill.png"): require('../images/user.png')}
+            style={styles.bottomTabIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -14,6 +100,26 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
   },
-})
+  bottomView: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 70,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  bottomTab: {
+    width: 20,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottomTabIcon: {
+    width: 24,
+    height: 24,
+  },
+});
