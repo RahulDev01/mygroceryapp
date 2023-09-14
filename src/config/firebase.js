7c0cb8d21a22727189013c2b161bf8c8de428108
Rahul Dev <rdev.dev01@gmail.com>
2023-09-14 22:07:08 +0530

    Firebase Setup

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJcBSOU21u7b7bHrfEwCzNr3E5be4In-4",
+  authDomain: "mygroceryapp-d5219.firebaseapp.com",
+  projectId: "mygroceryapp-d5219",
+  storageBucket: "mygroceryapp-d5219.appspot.com",
+  messagingSenderId: "286773933251",
+  appId: "1:286773933251:web:fc3b5f82230397eaf65e76",
+  measurementId: "G-NK2PYHFKRN",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
