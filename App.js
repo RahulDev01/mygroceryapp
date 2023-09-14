8221eb93bbeb71ef724dbe811654e6e63f51f522
Rahul Dev <rdev.dev01@gmail.com>
2023-09-14 22:06:18 +0530

    Firebase Setup

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import 'react-native-gesture-handler';
-import { StyleSheet} from "react-native";
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
 import AppNavigator from "./src/AppNavigator";
+// import app from "./src/config/firebase";
 
 export default function App() {
   return <AppNavigator />;
